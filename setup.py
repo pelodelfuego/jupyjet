3bb8e118132041a4437c826abe046634565f8a5c
pelodelfuego <clement.crepy@gmail.com>
2017-05-06 12:19:09 +0100

    dist working

--- setup.py
+++ setup.py
@@ -6,10 +6,13 @@ from distutils.core import setup
 
 setup(
     name='jupy-jet',
+    url='https://github.com/pelodelfuego/jupy-jet',
+    author='Clément CREPY',
+    author_email='clement.crepy@gmail.com',
     version='0.1.0',
-    packages=['jupyjet',],
+    py_modules=['jupyjet'],
     license='MIT',
-    long_description=open('README.md').read(),
+    long_description=open('README').read(),
 )
 
 
