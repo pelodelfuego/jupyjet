e2e012cfcfe94aa1933b63bc3293e96fd4a4b7f6
pelodelfuego <clement.crepy@gmail.com>
2017-05-06 12:06:58 +0100

    add first draft of setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+from distutils.core import setup
+
+setup(
+    name='jupy-jet',
+    version='0.1.0',
+    packages=['jupyjet',],
+    license='MIT',
+    long_description=open('README.md').read(),
+)
+
+
