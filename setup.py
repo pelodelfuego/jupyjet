9853318b18695d9032c6073ccefe2e3b66320d33
pelodelfuego <clement.crepy@gmail.com>
2017-05-07 11:42:32 +0100

    add long_description and dependancies

--- setup.py
+++ setup.py
@@ -4,6 +4,13 @@
 
 from distutils.core import setup
 
+long_description = """\
+Jupyjet is a IPyhton notebook extention which allow you to dynamically genrate python files from cells.
+
+Code portions registered with %jet or %jet-init magics will be written and updated into a python file.
+This approach make possible to easily separate a notebook into several notebooks and bind them.
+"""
+
 setup(
     name='jupy-jet',
     url='https://github.com/pelodelfuego/jupy-jet',
@@ -12,7 +19,10 @@ setup(
     version='0.1.0',
     py_modules=['jupyjet'],
     license='MIT',
-    long_description=open('README.md').read(),
+    description='jupyter extention to generate python modules',
+    long_description=long_description,
+    keywords = ['jupyter', 'notebook', 'module', 'jupyjet'],
+    install_required = ['IPython>=0.13', 'codegen==1.0'],
 )
 
 
