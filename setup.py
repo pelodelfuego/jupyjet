c766a56ea8e9f9e4ce5b340c70915912869419b4
pelodelfuego <clement.crepy@gmail.com>
2017-05-09 23:29:32 +0100

    update version

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(
     url='https://github.com/pelodelfuego/jupyjet',
     author='Clément CREPY',
     author_email='clement.crepy@gmail.com',
-    version='0.1.0',
+    version='0.2.0',
     py_modules=['jupyjet'],
     license='MIT',
     description='jupyter extention to generate python modules',
